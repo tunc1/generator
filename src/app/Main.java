09708a2bb2abc661b7e543ca95a4b9e9ccf69b2f
user <user>
2021-07-22 16:51:06 +0300

    Update Main Same ID Type Check

--- src/app/Main.java
+++ src/app/Main.java
@@ -7,6 +7,7 @@ public class Main
 {
     public static void main(String[] args) throws IOException
     {
+        String allIdType=null;
         Scanner scanner=new Scanner(System.in);
         System.out.println("Enter Project Path:");
         String projectPath=scanner.nextLine();
@@ -16,13 +17,24 @@ public class Main
         String entityPackage=scanner.nextLine();
         System.out.println("How many Entity Classes?");
         int entityClassQuantity=Integer.parseInt(scanner.nextLine());
+        System.out.println("Do all entity classes have same id type? (true/false)");
+        boolean sameIdType=Boolean.parseBoolean(scanner.nextLine());
+        if(sameIdType)
+        {
+            System.out.println("Enter Id Type:");
+            allIdType=scanner.nextLine();
+        }
         Entity[] entityClasses=new Entity[entityClassQuantity];
         for(int i=0;i<entityClasses.length;i++)
         {
             System.out.println("Enter Entity Class Name:");
             String name=scanner.nextLine();
-            System.out.println("Enter Entity ID Type:");
-            String idType=scanner.nextLine();
+            String idType=allIdType;
+            if(!sameIdType)
+            {
+                System.out.println("Enter Entity ID Type:");
+                idType=scanner.nextLine();
+            }
             entityClasses[i]=new Entity(name,idType);
         }
         Generator generator=new Generator();
