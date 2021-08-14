43e61ee51a72c7230eefdd4efddcb07d51f800d6
user <email>
2021-08-14 17:16:21 +0300

    Get Same Id Type for All Entity Classes

--- src/app/Main.java
+++ src/app/Main.java
@@ -7,7 +7,6 @@ public class Main
 {
     public static void main(String[] args) throws IOException
     {
-        String allIdType=null;
         Scanner scanner=new Scanner(System.in);
         System.out.println("Enter Project Path:");
         String projectPath=scanner.nextLine();
@@ -17,24 +16,13 @@ public class Main
         String entityPackage=scanner.nextLine();
         System.out.println("How many Entity Classes?");
         int entityClassQuantity=Integer.parseInt(scanner.nextLine());
-        System.out.println("Do all entity classes have same id type? (true/false)");
-        boolean sameIdType=Boolean.parseBoolean(scanner.nextLine());
-        if(sameIdType)
-        {
-            System.out.println("Enter Id Type:");
-            allIdType=scanner.nextLine();
-        }
+        System.out.println("Enter Id Type for All Entity Classes:");
+        String idType=scanner.nextLine();
         Entity[] entityClasses=new Entity[entityClassQuantity];
         for(int i=0;i<entityClasses.length;i++)
         {
-            System.out.println("Enter Entity Class Name:");
+            System.out.print("Enter Entity Class Name:");
             String name=scanner.nextLine();
-            String idType=allIdType;
-            if(!sameIdType)
-            {
-                System.out.println("Enter Entity ID Type:");
-                idType=scanner.nextLine();
-            }
             entityClasses[i]=new Entity(name,idType);
         }
         Generator generator=new Generator();
