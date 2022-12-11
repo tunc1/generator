2e7cf488b193a8b5396dd24e72879bfc59461c4a
tunc1 <email>
2022-12-11 16:13:43 +0300

    Remove ui

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,13 +1,29 @@
 package app;
 
 import app.generator.GeneratorFacade;
-import app.ui.GeneratorPresenter;
-import app.ui.GeneratorView;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main
 {
     public static void main(String[] args)
     {
-        new GeneratorPresenter(new GeneratorView(),new GeneratorFacade()).start();
+		GeneratorFacade generatorFacade=new GeneratorFacade();
+        String projectPath=args[0];
+        String basePackage=args[1];
+        String entityPackage=args[2];
+        List<String> entityNames=new LinkedList<>();
+        for(int i=3;i<args.length;i++)
+            entityNames.add(args[i]);
+        try
+        {
+            generatorFacade.generate(projectPath,basePackage,entityPackage,entityNames);
+        }
+        catch(IOException e)
+        {
+            System.out.println(e.getMessage());
+        }
     }
 }
